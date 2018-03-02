87b8663d921cbef9060663d46b9bb6b565e86161
daviddlaiton <ad.laiton10@uniandes.edu.co>
2018-03-02 14:30:10 -0500

    Environment var added

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-const express = require('express');
+
+//Express import
+const express = require("express");
 const app = express();
-const clientRoutes = require('./api/routes/client.js')
-const productRoutes = require('./api/routes/product.js');
-const authRoutes = require('./api/routes/auth.js');
-const adminRoutes = require('./api/routes/admin.js')
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://user:user@ds249418.mlab.com:49418/dcapan');
+//Paths
+const clientRoutes = require("./api/routes/client.js")
+const productRoutes = require("./api/routes/product.js");
+const authRoutes = require("./api/routes/auth.js");
+const adminRoutes = require("./api/routes/admin.js")
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+//Environment var 
+var config = require("./config");
+
+mongoose.connect(config.database);
 
 
-app.use(express.static(__dirname + '/public'));
-app.use(morgan('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(morgan("dev"));
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,21 +29,21 @@ app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+    );
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
-  }
-  next();
+}
+next();
 });
 
 
 
-app.use('/clients', clientRoutes);
-app.use('/products', productRoutes);
-app.use('/auth', authRoutes);
-app.use('/admin', adminRoutes);
+app.use("/clients", clientRoutes);
+app.use("/products", productRoutes);
+app.use("/auth", authRoutes);
+app.use("/admin", adminRoutes);
 
 
-app.listen(process.env.PORT || 5000);
-console.log('5000 is the magic port');
+app.listen(config.port);
+console.log("5000 is the magic port");
