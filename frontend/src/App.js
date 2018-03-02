8b3f1ccaed758e4b1d491633f406e1622a62fa9d
daviddlaiton <ad.laiton10@uniandes.edu.co>
2018-03-02 17:57:36 -0500

    React components added

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './dcapan_logo.png';
+import './css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
