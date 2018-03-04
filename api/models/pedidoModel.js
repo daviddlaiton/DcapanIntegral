08e6c41681b7f87212aefefdaa376284dc871f70
juanmanuel <jm.dominguez@uniandes.edu.co>
2018-03-04 01:50:13 -0500

    minorChanges

--- api/models/pedidoModel.js
+++ api/models/pedidoModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const pedidoSchema = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
-    fecha: String,
+    fechaPedido: String,
+    fechaEntrega: String,
     productos: [{type: mongoose.Schema.ObjectId,ref: "Producto", required: true}]
 });
 
--- api/routes/client.js
+++ api/routes/client.js
@@ -5,10 +5,11 @@ const Client = require('../models/clientModel.js');
 const jwt = require('jsonwebtoken');
 const Pedido = require('../models/pedidoModel.js');
 const Producto = require('../models/productoModel.js');
+const config = require("../../config");
 
 router.use(function(req, res, next){
     let token = req.body.token || req.query.token || req.headers['x-access-token'];
-    let superSecret = 'webDevUniandesSuperSecret';
+    let superSecret = config.secret;
     if(token){
         jwt.verify(token, superSecret, function(err, decoded){
             if(err){
@@ -38,7 +39,8 @@ router.get('/', (req, res, next) =>{
 });
 
 router.post('/:clientId/pedidos', (req, res, next) =>{
-    let pfecha = req.body.fecha;
+    let pfechaEntrega = req.body.fechaEntrega;
+    let pfechaPedido = req.body.pfechaPedido;
     let pProductos = req.body.productos;
     let productosFormateados = [];
     let p;
@@ -54,7 +56,8 @@ router.post('/:clientId/pedidos', (req, res, next) =>{
     
     let ppedido = new Pedido({
         _id: new mongoose.Types.ObjectId(),
-        fecha: pfecha,
+        fechaEntrega: pfechaEntrega,
+        fechaPedido: pfechaPedido,
         productos: productosFormateados
     
     });
